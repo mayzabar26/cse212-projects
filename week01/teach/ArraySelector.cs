53db615d0095987c410ecd9bf46fdbf499f41933
Mayza Barbosa <mayzabar26@gmail.com>
2025-07-03 15:55:02 -0300

    Problem 2: Solving a Complicated Problem

--- week01/teach/ArraySelector.cs
+++ week01/teach/ArraySelector.cs
@@ -3,14 +3,35 @@ public static class ArraySelector
     public static void Run()
     {
         var l1 = new[] { 1, 2, 3, 4, 5 };
-        var l2 = new[] { 2, 4, 6, 8, 10};
-        var select = new[] { 1, 1, 1, 2, 2, 1, 2, 2, 2, 1};
+        var l2 = new[] { 2, 4, 6, 8, 10 };
+        var select = new[] { 1, 1, 1, 2, 2, 1, 2, 2, 2, 1 };
         var intResult = ListSelector(l1, l2, select);
         Console.WriteLine("<int[]>{" + string.Join(", ", intResult) + "}"); // <int[]>{1, 2, 3, 2, 4, 4, 6, 8, 10, 5}
     }
 
     private static int[] ListSelector(int[] list1, int[] list2, int[] select)
     {
-        return [];
+        int[] result = new int[select.Length];
+        int i1 = 0; //store list 1
+        int i2 = 0; //store list 2
+
+        for (int i = 0; i < select.Length; i++)
+        {
+            if (select[i] == 1)
+            {
+                result[i] = list1[i1];
+                i1++;
+            }
+            else if (select[i] == 2)
+            {
+                result[i] = list2[i2];
+                i2++;
+            }
+
+        }
+
+        return result;
     }
 }
+
+
