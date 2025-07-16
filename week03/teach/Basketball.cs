caa399c92555935fe26579eb76dbeb9c1c15f9cb
Mayza Barbosa <mayzabar26@gmail.com>
2025-07-16 18:47:46 -0300

    W03 Team Activity: Sets and Maps

--- week03/teach/Basketball.cs
+++ week03/teach/Basketball.cs
@@ -17,20 +17,38 @@ public class Basketball
 {
     public static void Run()
     {
+        //Creates a dictionary to store the total points for each player (player ID (key), total points (value)
         var players = new Dictionary<string, int>();
 
         using var reader = new TextFieldParser("basketball.csv");
         reader.TextFieldType = FieldType.Delimited;
         reader.SetDelimiters(",");
         reader.ReadFields(); // ignore header row
-        while (!reader.EndOfData) {
+        while (!reader.EndOfData)
+        {
             var fields = reader.ReadFields()!;
             var playerId = fields[0];
             var points = int.Parse(fields[8]);
+
+            //if player alredy exists in the dictionary, add points to their total
+            if (players.ContainsKey(playerId))
+                players[playerId] += points;
+
+            else
+                //if not, add new entry for the player with their points
+                players[playerId] = points;
         }
+        //After reading data, convert the dict. to an array and sort it
+        var topPlayers = players.ToArray();
+        //Sorts array (highest first)
+        Array.Sort(topPlayers, (p1, p2) => p2.Value - p1.Value);
 
-        Console.WriteLine($"Players: {{{string.Join(", ", players)}}}");
-
-        var topPlayers = new string[10];
+        Console.WriteLine();
+        for (var i = 0; i < 10; i++)
+        {
+            //Prints the top the players with highest scores
+            Console.WriteLine(topPlayers[i]);
+        }
+        
     }
 }
